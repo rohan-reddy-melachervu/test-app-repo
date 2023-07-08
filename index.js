c38e9323db7c07b11ab6f051ca8ae1d3299ba7fe
Yash Kadam <yashkadam278@gmail.com>
2023-07-08 10:13:53 +0530

    JS calc

---
+++ index.js
@@ -0,0 +1,23 @@
+var a = 1;
+
+var b = "1";
+
+console.log("====================================");
+console.log(a == b);
+console.log(a === b);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log((a += b));
+
+console.log(a++);
+
+console.log(a--);
+
+console.log(a > b);
+console.log(a < b);
+console.log("====================================");
