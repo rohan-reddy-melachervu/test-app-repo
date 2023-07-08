7b241750206472926ca4f78480821e8fe3e6e8d6
rohan-jatya <rohan@jatyatech.com>
2023-07-08 18:52:25 +0530

    Edited to array and added Harsh

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-app = {
+app = [ {
     'name': 'Rohan',
     'age': 22,
     'area': ['Blr', 'Hyd'],
     'school': ['NPS', 'DPS', 'VIT'],
     'jobs': ['AP Govm', 'Webknot'],
+},
+{
+    'name':'Harsh'
 }
+]
