8a2b1dc04b38c0934d9267dbc176dc3f03557d04
rohan-reddy-melachervu <121099797+rohan-reddy-melachervu@users.noreply.github.com>
2023-07-10 12:08:50 +0530

    Test PR  (#16)
    * Modified hello.txt
    
    * Modified hello.txt
    
    * Modified hello.txt
    
    * Modified hello.txt
    
    * Modified hello.txt
    
    * Added yash and harsh last names
    
    * Added index.js
    
    * Added app.js
    
    * Edited app.js
    
    * Added age field in app.js
    
    * Added areas in app.js
    
    * Added schools in app.js
    
    * Added more schools in app.js
    
    * Modified app.js schools array
    
    * Added jobs field in app.js
    
    * Added jobs till now in jobs array
    
    * Edited to array and added Harsh
    
    * Added Harsh age
    
    * Edited Harsh age
    
    * Added Harsh area
    
    * Added schools field and added Harsh's school in the school array
    
    * Added hobbies field in app.js for Rohan user
    
    * Added Boxes array in index.js to have an array of Box components and added Cardboard Box as first element
    
    * Added wooden box to boxes array
    
    ---------
    
    Co-authored-by: rohan-jatya <rohan@jatyatech.com>

---
+++ app.js
@@ -0,0 +1,15 @@
+app = [ {
+    'name': 'Rohan',
+    'age': 22,
+    'area': ['Blr', 'Hyd'],
+    'school': ['NPS', 'DPS', 'VIT'],
+    'hobbies': ['Tennis'],
+    'jobs': ['AP Govm', 'Webknot'],
+},
+{
+    'name':'Harsh',
+    'age': 22,
+    'area': 'Raipur',
+    'schools': ['NIT'],
+}
+]
---
+++ index.js
@@ -0,0 +1,10 @@
+export default Boxes = [
+    {
+        'id': 1,
+        'name': 'cardboard',
+    },
+    {
+        'id': 2,
+        'name': 'wooden',
+    },
+]
